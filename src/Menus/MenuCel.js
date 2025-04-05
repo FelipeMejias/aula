b5639bddaf86fb64252be5ff4df26ac24d4f0935
Felipe Mejias <felipe.mejias50@gmail.com>
2025-04-05 15:10:13 -0300

    cssBrabo

--- src/Menus/MenuCel.js
+++ src/Menus/MenuCel.js
@@ -36,7 +36,7 @@ display:flex;align-items:center;
 const Tudo=styled.article`
 width:100%;height:60px;
 justify-content:center;
-background-color:#2A0145;
+background-color:#5D7219;
 display:none;
 
 @media(max-width:750px){
