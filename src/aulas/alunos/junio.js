31240f76b4259759d4af96dd1b7dba2628c91877
Felipe Mejias <felipe.mejias50@gmail.com>
2025-04-22 15:38:18 -0300

    junioEmatheus

--- src/aulas/alunos/junio.js
+++ src/aulas/alunos/junio.js
@@ -35,9 +35,36 @@ A função deve calcular o valor do desconto e mostrar 2 frases no console:
 -A economia foi de R$ 300
     `},
     ]
+const aula2=[
+    {linguagem:'javascript',nome:'Revisão Boolean',icone:js,caminho:formarTexto(2,1)},
+    {linguagem:'javascript',nome:'Funções aninhadas',icone:js,caminho:formarTexto(2,2)},
+    {linguagem:'javascript',nome:'Return',icone:js,caminho:formarTexto(2,3)},
+    {linguagem:'javascript',nome:'Funções com return aninhadas',icone:js,caminho:formarTexto(2,4)},
+    {linguagem:'javascript',nome:'Reaproveitando função',icone:js,caminho:formarTexto(2,5)},
+
+    {linguagem:'javascript',nome:'EXERCÍCIO 1',icone:notass,texto:`
+Faça uma função que recebe: o valor de um produto
+-Caso o valor seja maior que 100, a função retorna o valor com desconto de 20%
+-Caso o valor seja menor que 100, a função retorna o valor sem desconto
+    `},{linguagem:'javascript',nome:'EXERCÍCIO 2',icone:notass,texto:`
+Agora, a função deve receber 2 parâmetros:
+-valor do produto
+-se o cliente tem desconto ou não (true ou false)
+A função deve retornar o valor inteiro ou os 20% de desconto baseado no valor do 2º parâmetro
+    `},{linguagem:'javascript',nome:'EXERCÍCIO 3',icone:notass,texto:`
+Dessa vez, o segundo parâmetro será o grau de fidelidade do cliente com a loja, os parâmetros serão:
+-valor do produto
+-"A" ou "B" ou "C" ou false
+Caso o cliente receba false como parâmetro, é sua primeira compra, ele não tem desconto
+Os clientes A têm 30%, os clientes B têm 20%, e os clientes C têm 10% de desconto
+    `},
+    ]
 export const junio=[
     {
         nome:'Aula 1',
         arquivos:aula1
+    },{
+        nome:'Aula 2',
+        arquivos:aula2
     },
 ]
